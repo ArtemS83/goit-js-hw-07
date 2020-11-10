8a1ad3ef62159acf04bae9748d6a40fafd7fefc3
Atrem Sergieiev <a_serg83@ukr.net>
2020-11-10 16:36:55 +0100

    Update task-7_(2,3)

--- js/task-7_2.js
+++ js/task-7_2.js
@@ -6,6 +6,37 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+/*========Вариант создания "в ручную" при помощи document.createElement()=========*/
+
+// const ingredientsRef = document.querySelector('#ingredients');
+// const li1 = document.createElement('li');
+// li1.textContent = ingredients[0];
+// const li2 = document.createElement('li');
+// li2.textContent = ingredients[1];
+// const li3 = document.createElement('li');
+// li3.textContent = ingredients[2];
+// const li4 = document.createElement('li');
+// li4.textContent = ingredients[3];
+// const li5 = document.createElement('li');
+// li5.textContent = ingredients[4];
+// const li6 = document.createElement('li');
+// li6.textContent = ingredients[5];
+// ingredientsRef.append(li1, li2, li3, li4, li5, li6);
+
+/*========Вариант создания через цикл for при помощи document.createElement()=========*/
+
+// const ingredientsRef = document.querySelector('#ingredients');
+// const elements = [];
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const liEl = document.createElement('li');
+//   liEl.textContent = ingredients[i];
+//   elements.push(liEl);
+// }
+
+// ingredientsRef.append(...elements);
+
+/*========Вариант создания через .map при помощи document.createElement()========*/
 
 const ingredientsRef = document.querySelector('#ingredients');
 const createElement = el => {
--- js/task-7_3.js
+++ js/task-7_3.js
@@ -15,16 +15,50 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+/*========Вариант создания через .map при помощи document.createElement().=========*/
+
+// const galleryRef = document.querySelector('#gallery');
+
+// const createImageRef = el => {
+//   const itemRef = document.createElement('li');
+//   const imageRef = document.createElement('img');
+//   imageRef.classList.add('image-item');
+//   imageRef.src = el.url;
+//   imageRef.alt = el.alt;
+//   itemRef.appendChild(imageRef);
+//   return itemRef;
+// };
+// const imageGalaryRef = images.map(createImageRef);
+// galleryRef.append(...imageGalaryRef);
+
+/*========Вариант создания через .reduce  при помощи .innerHTML=========*/
+
+// const galleryRef = document.querySelector('#gallery');
+
+// const itemsMarkup = images.reduce(
+//   (string, item) =>
+//     string +
+//     `<li>
+//     <img class="image-item" src=${item.url} alt=${item.alt}>
+//     </li>`,
+//   '',
+// );
+// galleryRef.innerHTML = itemsMarkup;
+// // galleryRef.innerHTML = ''; //очистка списка
+
+/*========Вариант создания через функцию при помощи .insertAdjacentHTML(position, string)=========*/
 
 const galleryRef = document.querySelector('#gallery');
-const createImageRef = el => {
-  const itemRef = document.createElement('li');
-  const imageRef = document.createElement('img');
-  imageRef.classList.add('image-item');
-  imageRef.src = el.url;
-  imageRef.alt = el.alt;
-  itemRef.appendChild(imageRef);
-  return itemRef;
-};
-const imageGalaryRef = images.map(createImageRef);
-galleryRef.append(...imageGalaryRef);
+
+function getItemsMarkup(arr) {
+  return arr.reduce(
+    (string, { url, alt }) =>
+      string +
+      `<li>
+    <img class="image-item"  src=${url} alt='${alt}'>
+    </li>`,
+    ` `,
+  );
+}
+
+galleryRef.insertAdjacentHTML('beforeend', getItemsMarkup(images));
